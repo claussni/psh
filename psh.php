12d751fb8af5dc93f62ccb298793eee879a894c3
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2011-11-25 14:34:27 +0100

    embedded code in class __PSH to avoid namespace pollution

--- psh.php
+++ psh.php
@@ -17,98 +17,97 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$silent_exit=FALSE;
-$sockets=array();
+class __PSH {
 
-define('CHILD_EXIT', 0);
-define('CHILD_OK', 1);
-define('CHILD_CRASH', 2);
+	const CHILD_EXIT = 0;
+	const CHILD_OK = 1;
+	const CHILD_CRASH = 2;
 
-function set_silent_exit($flag) {
-	global $silent_exit;
-	$silent_exit = $flag;
-}
+	private static $silent_exit=FALSE;
+	private static $sockets=array();
 
-function shutdown() {
-	global $sockets;
-	$err = error_get_last();
-	if ($err) {
-		switch ($err['type']) {
-			case E_ERROR:
-			case E_PARSE:
-			case E_CORE_ERROR:
-			case E_USER_ERROR:
-			case E_COMPILE_ERROR:
-				socket_write($sockets[2], CHILD_CRASH);
-				exit;
-		}
+	private static function set_silent_exit($flag) {
+		self::$silent_exit = $flag;
 	}
-	global $silent_exit;
-	if (!$silent_exit) socket_write($sockets[2], CHILD_EXIT);
-}
 
-
-function exec_srv($statement=NULL) {
-	global $sockets;
-recursion:
-	if ($statement != NULL) {
-		eval($statement);
+	public static function shutdown() {
 		$err = error_get_last();
-		if (($err) && ($err['type'] == E_PARSE)) {
-			exit;
+		if ($err) {
+			switch ($err['type']) {
+				case E_ERROR:
+				case E_PARSE:
+				case E_CORE_ERROR:
+				case E_USER_ERROR:
+				case E_COMPILE_ERROR:
+					socket_write(self::$sockets[2], self::CHILD_CRASH);
+					exit;
+			}
 		}
-		socket_write($sockets[2], CHILD_OK);
+		if (!self::$silent_exit) socket_write(self::$sockets[2], self::CHILD_EXIT);
 	}
 
-	while (1) {
-		$statement = socket_read($sockets[1], 300, PHP_NORMAL_READ);
-		switch (pcntl_fork()) {
-			case -1:
-				echo "Forking failed. Statement not executed.\n";
-				$status = CHILD_CRASH;
+	private static function exec_srv($statement=NULL) {
+	recursion:
+		if ($statement != NULL) {
+			eval($statement);
+			$err = error_get_last();
+			if (($err) && ($err['type'] == E_PARSE)) {
 				exit;
+			}
+			socket_write(self::$sockets[2], self::CHILD_OK);
+		}
+
+		while (1) {
+			$statement = socket_read(self::$sockets[1], 300, PHP_NORMAL_READ);
+			switch (pcntl_fork()) {
+				case -1:
+					echo "Forking failed. Statement not executed.\n";
+					$status = self::CHILD_CRASH;
+					exit;
+				case 0:
+					goto recursion;
+			}
+			$status = socket_read(self::$sockets[3], 1);
+			socket_write(self::$sockets[1], $status);
+			self::set_silent_exit($status == self::CHILD_OK);
+			if ($status != self::CHILD_CRASH) exit;
+			pcntl_wait($_st);
+		}
+	}
+
+	private static function shell() {
+		switch(pcntl_fork()) {
+			case -1:
+				die("Cannot fork");
 			case 0:
-				goto recursion;
+				self::exec_srv();
+				exit();
 		}
-		$status = socket_read($sockets[3], 1);
-		socket_write($sockets[1], $status);
-		set_silent_exit($status == CHILD_OK);
-		if ($status != CHILD_CRASH) exit;
-		pcntl_wait($_st);
+		while (1) {
+			$line = readline("psh > ");
+			if ($line == "") continue;
+			readline_add_history($line);
+			socket_write(self::$sockets[0], $line."\n");
+			$rec = socket_read(self::$sockets[0], 1);
+				if ($rec == self::CHILD_EXIT) {
+				break;
+			}
+		}
+		self::set_silent_exit(TRUE);
 	}
+
+	public static function run() {
+		socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $pair1);
+		socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $pair2);
+		self::$sockets = array_merge($pair1, $pair2);
+		register_shutdown_function(array('__PSH', 'shutdown'));
+		self::shell();
+		socket_close(self::$sockets[0]);
+		socket_close(self::$sockets[1]);
+		socket_close(self::$sockets[2]);
+		socket_close(self::$sockets[3]);
+	}
+
 }
 
-
-function shell() {
-	global $sockets;
-	switch(pcntl_fork()) {
-		case -1:
-			die("Cannot fork");
-		case 0:
-			exec_srv();
-			exit();
-	}
-	while (1) {
-		$line = readline("psh > ");
-		if ($line == "") continue;
-		readline_add_history($line);
-		socket_write($sockets[0], $line."\n");
-		$rec = socket_read($sockets[0], 1);
-	      	if ($rec == CHILD_EXIT) {
-			break;
-		}
-	}
-	set_silent_exit(TRUE);
-}
-
-
-socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $pair1);
-socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $pair2);
-$sockets = array_merge($pair1, $pair2);
-register_shutdown_function('shutdown');
-shell();
-socket_close($sockets[0]);
-socket_close($sockets[1]);
-socket_close($sockets[2]);
-socket_close($sockets[3]);
-
+__PSH::run();
